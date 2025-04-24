e6980ac12656d40f7cfc32f74b8892a3e05ff5d9
1i-2022-Kacper-24 <1i.2022.Kacper.24@gmail.com>
2025-04-24 12:16:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const display = document.querySelector('main');
 const btn = document.querySelector('button');
+const t1 = document.querySelector('#t1');
+const t2 = document.querySelector('#t2');
+
+let i = 1;
 
 btn.addEventListener('click', function(){
-    const par = document.createElement('p');
-    par.innerText = 'nowy paragraf';
+    const par = document.createElement(t1.value);
+    par.innerText = `${t2.value}`;
+    par.addEventListener('click', function(){
+        this.remove()
+    })
     display.append(par);
+    i = i + 1;
 })
